32e99f713b90093ab3e368b4f24ffe517a4541aa
BerniWittmann <wittmann_b@web.de>
2017-06-13 22:34:17 +0200

    redesigned home page

---
+++ src/layouts/fullWidth.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-navigation class="no-nav-margin"></v-navigation>
+
+    <div class="container-fluid">
+      <!-- Content will be placed here -->
+      <slot></slot>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  /* ============
+   * Default Layout
+   * ============
+   *
+   * Used for the home and account pages.
+   *
+   * Layouts are used to store a lot of shared code.
+   * This way the app stays clean.
+   */
+
+  export default {
+    components: {
+      VNavigation: require('@/components/navigation.vue'),
+    },
+  };
+
+</script>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,30 +1,61 @@
 <template>
   <v-layout>
-    <v-panel
-      contextual-style="primary"
-    >
-      <h1 class="panel-title" slot="heading">
-        {{ $t("general.welcome") }}
-      </h1>
-      <div slot="body">
+    <div class="row home-part home-part-image">
+      <div class="home-header-image">
+      </div>
+    </div>
+
+    <div class="row home-part">
+      <div class="col-md-12">
+        <h2>Bringt Leben an den Esstisch</h2>
+        <h4>Mit Mampf essen Sie gemeinsam und sparen dabei sogar noch</h4>
+        <router-link
+          :to="{ name: 'meals.index' }"
+          class="btn btn-primary"
+          tag="a"
+        >Essen finden
+        </router-link>
+      </div>
+    </div>
+
+    <div class="row home-part home-part-split-vertical">
+      <div class="col-md-6">
+        <div class="home-subheader-image"></div>
+      </div>
+      <div class="col-md-6">
+        <h3>Gemeinsam essen lohnt sich</h3>
+        <p class="lead">Essen anbieten. Gäste einladen. Kosten sparen</p>
+        <router-link
+          :to="{ name: 'meals.add' }"
+          class="btn btn-primary"
+          tag="a"
+        >Essen anbieten
+        </router-link>
+      </div>
+    </div>
+    <div class="row home-part">
+      <div class="col-md-12">
+        <h3>{{ $t("general.welcome") }}</h3>
         <p>
           {{ $t("general.welcome_text") }}
         </p>
         <p>
           {{ $t("general.bug_notice") }}
         </p>
-        <a class="btn btn-primary" href="https://github.com/BerniWittmann/mampf-prototype/issues" target="_blank">
-          <i class="fa fa-github fa-fw" aria-hidden="true"></i>
-          <span class="padding-left">{{ $t("general.github") }}</span>
-        </a>
-        <a class="btn btn-primary" href="mailto:b.wittmann@mail.de">
-          <i class="fa fa-envelope fa-fw" aria-hidden="true"></i>
-          <span class="padding-left">{{ $t("general.email") }}</span>
-        </a>
+        <div>
+          <a class="btn btn-primary" href="https://github.com/BerniWittmann/mampf-prototype/issues" target="_blank">
+            <i class="fa fa-github fa-fw" aria-hidden="true"></i>
+            <span class="padding-left">{{ $t("general.github") }}</span>
+          </a>
+          <a class="btn btn-primary" href="mailto:b.wittmann@mail.de">
+            <i class="fa fa-envelope fa-fw" aria-hidden="true"></i>
+            <span class="padding-left">{{ $t("general.email") }}</span>
+          </a>
+        </div>
+        <br>
+        <p v-html="$t('general.by_text')"></p>
       </div>
-      <div slot="footer" v-html="$t('general.by_text')">
-      </div>
-    </v-panel>
+    </div>
   </v-layout>
 </template>
 
@@ -38,8 +69,7 @@
 
   export default {
     components: {
-      VLayout: require('@/layouts/default.vue'),
-      VPanel: require('@/components/panel.vue'),
+      VLayout: require('@/layouts/fullWidth.vue'),
     },
   };
 </script>
