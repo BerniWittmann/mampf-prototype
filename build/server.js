9ab9af531e4db5413898e78203a696821eb82a7b
BerniWittmann <wittmann_b@web.de>
2017-06-25 19:24:47 +0200

    added favicon

--- build/server.js
+++ build/server.js
@@ -1,6 +1,12 @@
 var connect = require('connect');
+var favicon = require('serve-favicon');
+var path = require('path');
 var serveStatic = require('serve-static');
 var port = process.env.PORT || 8080;
-connect().use(serveStatic(__dirname + '/../dist/')).listen(port, function(){
+
+var app = connect();
+app.use(favicon(path.join(__dirname, '../dist/static', 'favicon.ico')));
+app.use(serveStatic(__dirname + '/../dist/'));
+app.listen(port, function(){
   console.log('Server running on ' + port + ' ...');
 });
